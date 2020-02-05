bc701d697f9e26862e895332caffa98cda6fd9c9
yyuguang <790335255@qq.com>
2020-02-05 15:12:34 +0800

    力扣 - 数公用TreeNode

---
+++ src/com/lnzz/数/TreeNode.java
@@ -0,0 +1,19 @@
+package com.lnzz.数;
+
+/**
+ * ClassName：TreeNode
+ *
+ * @author 冷暖自知
+ * @version 1.0
+ * @date 2020/2/5 15:10
+ * @Description:
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
