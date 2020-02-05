48450b2882d609826282730430c60a00bfa46159
yyuguang <790335255@qq.com>
2020-02-05 11:36:59 +0800

    力扣 - 链表公用ListNode

---
+++ src/com/lnzz/链表/ListNode.java
@@ -0,0 +1,18 @@
+package com.lnzz.链表;
+
+/**
+ * ClassName：ListNode
+ *
+ * @author 冷暖自知
+ * @version 1.0
+ * @date 2020/1/31 14:38
+ * @Description:
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
