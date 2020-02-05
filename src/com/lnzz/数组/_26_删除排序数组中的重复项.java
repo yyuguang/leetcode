effc2456ff491d95f00c45fe90a72173b7c9ab90
yyuguang <790335255@qq.com>
2020-02-05 11:38:08 +0800

    力扣 - 26.删除排序数组中的重复项

---
+++ src/com/lnzz/数组/_26_删除排序数组中的重复项.java
@@ -0,0 +1,34 @@
+package com.lnzz.数组;
+
+/**
+ * ClassName：_26_删除排序数组中的重复项
+ *
+ * @author 冷暖自知
+ * @version 1.0
+ * @date 2020/1/31 11:57
+ * @Description:
+ */
+public class _26_删除排序数组中的重复项 {
+    /**
+     * 26.给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+     * 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+
+    public static int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+}
